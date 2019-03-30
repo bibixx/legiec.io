e6e7ced96ef3b699b2bbf4a779dcb5597fd04e4d
Bartosz Legięć <bartosz@legiec.eu>
2019-03-30 10:30:44 +0100

    Update restart.sh

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 git fetch --all &&
-git reset --hard origin/master &&
-mkdir -p /home/www/html/root &&
-rm -rf /home/www/html/root/* &&
-rsync --quiet -av --progress --exclude="bin" ./ /home/www/html/root/
+git reset --hard origin/master
