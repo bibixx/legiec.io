529b44867990425f18dffb5c48b7f45fd1144014
Bartosz Legięć <bartosz@legiec.io>
2023-12-26 21:40:12 +0100

    Refactor to Next.js w/ server actions + Vercel DB

--- types/Airtable.ts
+++
@@ -1,6 +0,0 @@
-export type UrlRow = {
-  url: string;
-  slug: string;
-  active: boolean;
-  createdAt: string;
-};
--- utils/validators/createUrl.utils.ts
+++
@@ -1,3 +0,0 @@
-export const SLUG_ALLOWED_CHARACTERS = 'a-zA-Z0-9-._';
-export const isValidSlug = (slug: string) =>
-  new RegExp(`^[${SLUG_ALLOWED_CHARACTERS}]+$`).test(slug);
